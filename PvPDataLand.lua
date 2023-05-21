55afeb0475aa087d44bddd898a1d85b3d3887fe9
Endogenous-10 <39071651+Endogenous-10@users.noreply.github.com>
2023-05-21 13:00:42 -0400

    Update PvPDataLand.lua

--- PvPDataLand.lua
+++ PvPDataLand.lua
@@ -1,315 +1,126 @@
--- PvPDataLand.lua
--- A World of Warcraft add-on for PvP data analysis
--- Author: Endogenous-10
--- Version: 1.0
+-- Create the addon frame
+local frame = CreateFrame("Frame")
 
--- Define slash command
-SLASH_PVPDATA1 = "/pvpdata"
+-- Initialize the database
+local PvPDataLandDB = {}
 
--- Register slash command handler
-SlashCmdList["PVPDATA"] = function(msg)
-  -- Open GUI here
-  local player = UnitName("player") -- Get local player
-  local frame = CreateFrame("Frame", "PvPDataLandFrame", UIParent)
-  frame:SetSize(200, 200)
-  frame:SetPoint("CENTER")
-  frame:Show()
-end
+-- Create a separate table for combat log data
+local CombatLogData = {}
 
--- Event handling function
-local function handleEvent(self, event, ...)
-  if event == "PLAYER_ENTERING_WORLD" then
-    local player = UnitName("player")
-    -- Do any necessary initialization here
-  end
-  -- Handle other events here
-end
+-- Register the events we want to track
+frame:RegisterEvent("ARENA_MATCH_START")
+frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+frame:RegisterEvent("ARENA_MATCH_END")
+frame:RegisterEvent("PERSONAL_RATED_INFO")
 
--- Create a frame to handle events
-local frame = CreateFrame("Frame", "PvPDataLandEventFrame")
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-frame:SetScript("OnEvent", handleEvent)
+-- This variable will store the current instance ID
+local currentInstanceID
 
--- PvPDataLand events handling
+-- This function will handle events
 frame:SetScript("OnEvent", function(self, event, ...)
-    if event == "PLAYER_ENTERING_WORLD" then
-        -- Code to handle the PLAYER_ENTERING_WORLD event
+    if event == "ARENA_MATCH_START" then
+        local instanceID, _, matchType, teamId = ...
+        currentInstanceID = instanceID
 
-        -- Retrieve the character and server information from the World of Warcraft client
-        local realmName = GetRealmName()
-        local characterName = UnitName("player")
+        -- Get the timestamp for the start of the arena match
+        local startTimeStamp = GetServerTime()
 
-        -- Define the path to the REFlex LUA file based on the character and server information
-        local reflexFilePath = string.format("WTF\\Account\\%s\\%s\\SavedVariables\\REFlex.lua", realmName, characterName)
+        -- Initialize a new table to hold the data for this match
+        PvPDataLandDB[instanceID] = {
+            start = startTimeStamp,
+            matchType = matchType,
+            players = {},
+        }
 
-        -- Modify the export file path to include the character and server information
-        local exportFilePath = string.format("WTF\\Account\\%s\\%s\\SavedVariables\\PvPDataExport.lua", realmName, characterName)
+        -- Record the race, class, spec, and talent tree of all players in the arena
+        for i = 1, GetNumGroupMembers() do
+            local unit = "party"..i
+            if i == GetNumGroupMembers() then unit = "player" end
+            local name, realm = UnitName(unit)
+            local raceName, _, raceID = UnitRace(unit)
+            local className, _, classId = UnitClass(unit)
+            local specId = GetInspectSpecialization(unit)
+            local isEnemy = IsEnemy(name, realm)
 
-        -- Open the REFlex LUA file for reading
-        local reflexFile = io.open(reflexFilePath, "r")
-
-        if reflexFile then
-            print("Reflex file opened successfully.")
-
-            -- Read the entire content of the REFlex file
-            local reflexContent = reflexFile:read("*a")
-
-            -- Close the REFlex file
-            reflexFile:close()
-
-            print("Reflex file read successfully.")
-
-            -- Extract the historical data from the REFlex content
-            local historicalData = extractDataFromReflex(reflexContent)
-
-            print("Historical data extracted from REFlex.")
-
-            -- Export the historical data in a format compatible with PvPDataTracker
-            local exportData = convertDataForPvPDataTracker(historicalData)
-
-            print("Historical data converted for PvPDataTracker.")
-
-            -- Save the export data to a file that can be imported into PvPDataTracker
-            local exportFile = io.open(exportFilePath, "w")
-
-            if exportFile then
-                print("Export file opened successfully.")
-
-                -- Write the export data to the export file
-                exportFile:write("return " .. serialize(exportData))
-
-                -- Close the export file
-                exportFile:close()
-
-                print("Export successful. The data has been exported to '" .. exportFilePath .. "'.")
-            else
-                print("Failed to create export file.")
-            end
-        else
-            print("Failed to open Reflex file.")
-        end
-
-        -- Utility function to serialize Lua table as a string
-        local function serialize(tbl)
-            local str = "{"
-            for k, v in pairs(tbl) do
-                str = str .. "[" .. tostring(k) .. "]=" .. serializeValue(v) .. ","
-            end
-            str = str .. "}"
-            return str
-        end
-
-        -- Utility function to serialize a value
-        local function serializeValue(value)
-            if type(value) == "string" then
-                return string.format("%q", value)
-            elseif type(value) == "table" then
-                return serialize(value)
-            else
-                return tostring(value)
-            end
-        end
-
-        -- Replace the following functions with your own extraction and conversion logic based on the format of data in REFlex
-        local function extractDataFromReflex(reflexContent)
-            -- Extract and return historical data from REFlex content
-            print("Extracting historical data from REFlex...")
-            -- Add your extraction logic here
-        end
-
-        local function convertDataForPvPDataTracker(historicalData)
-            -- Convert and return historical data in a format compatible with PvPDataTracker
-            print("Converting historical data for PvPDataTracker...")
-            -- Add your conversion logic here
-        end
-
-        -- Define the path to the exported data file based on the character and server information
-        local exportFilePath = string.format("WTF\\Account\\%s\\%s\\SavedVariables\\PvPDataExport.lua", realmName, characterName)
-
-        -- Function to import the data from the exported file
-        local function importData()
-            -- Load the exported data file
-            local exportedData = dofile(exportFilePath)
-
-            print("Importing data from PvPDataExport.lua...")
-
-            -- Process the imported data in your PvPDataTracker addon
-            -- Here, you can update the pvpData table or perform any other necessary actions based on the imported data
-
-            -- Example: Update the pvpData table with the imported data
-            pvpData = exportedData
-
-            print("Data imported successfully.")
-        end
-
-        -- Call the importData function to import the exported data into your PvPDataTracker addon
-        importData()
-
-        local function updateGUI(data)
-            -- Format the data as a string
-            local text = string.format(
-                "Wins: %d\nLosses: %d\nWin rate: %.2f%%\nCurrent rating: %d\nHighest rating: %d\nLowest rating: %d\n",
-                data.stats.wins,
-                data.stats.losses,
-                data.stats.winRate,
-                data.stats.currentRating,
-                data.stats.highestRating,
-                data.stats.lowestRating
-            )
-            -- Set the text of the label
-            dataLabel:SetText(text)
-        end
-
-        local function addMatch(matchData)
-            table.insert(pvpData.matches, matchData)
-
-            local mode = matchData.mode
-            table.insert(pvpData.modes[mode].matches, matchData)
-
-            updateStats()
-
-            -- Update the GUI with the new data for the selected mode
-            local modeDropdownValue = modeDropdown:GetValue()
-            local modeKey = modeDropdown:GetList()[modeDropdownValue]
-            updateGUI(pvpData.modes[modeKey])
-        end
-
-        local function updateStats()
-            -- Reset global stats
-            pvpData.stats.wins = 0
-            pvpData.stats.losses = 0
-            pvpData.stats.currentRating = 0
-            pvpData.stats.highestRating = 0
-            pvpData.stats.lowestRating = 0
-
-            -- Reset mode-specific stats
-            for modeName, modeData in pairs(pvpData.modes) do
-                modeData.stats.wins = 0
-                modeData.stats.losses = 0
-                modeData.stats.currentRating = 0
-                modeData.stats.highestRating = 0
-                modeData.stats.lowestRating = 0
+            -- Get talent tree information
+            local talentTree = {}
+            for j = 1, GetMaxTalentTier() do
+                local talentId, _, _, _, _, selected = GetTalentInfo(j, GetActiveSpecGroup())
+                talentTree[j] = {
+                    talentId = talentId,
+                    selected = selected,
+                }
             end
 
-            -- Compute stats from all matches
-            for _, matchData in ipairs(pvpData.matches) do
-                local mode = matchData.mode
-                local modeStats = pvpData.modes[mode].stats
-                local globalStats = pvpData.stats
-
-                if matchData.result == "win" then
-                    modeStats.wins = modeStats.wins + 1
-                    globalStats.wins = globalStats.wins + 1
-                else
-                    modeStats.losses = modeStats.losses + 1
-                    globalStats.losses = globalStats.losses + 1
-                end
-
-                modeStats.currentRating = matchData.rating
-                globalStats.currentRating = globalStats.currentRating + matchData.rating
-
-                if matchData.rating > modeStats.highestRating then
-                    modeStats.highestRating = matchData.rating
-                    end
-                end
-                if matchData.rating < modeStats.lowestRating or modeStats.lowestRating == 0 then
-                    modeStats.lowestRating = matchData.rating
-                end
-                if matchData.rating > globalStats.highestRating then
-                    globalStats.highestRating = matchData.rating
-                end
-                if matchData.rating < globalStats.lowestRating or globalStats.lowestRating == 0 then
-                    globalStats.lowestRating = matchData.rating
-                end
-            end
-
-            -- Compute win rates
-            for modeName, modeData in pairs(pvpData.modes) do
-                local modeStats = modeData.stats
-
-                if modeStats.wins + modeStats.losses > 0 then
-                    modeStats.winRate = modeStats.wins / (modeStats.wins + modeStats.losses) * 100
-                else
-                    modeStats.winRate = 0
-                end
-            end
-
-            if pvpData.stats.wins + pvpData.stats.losses > 0 then
-                pvpData.stats.winRate = pvpData.stats.wins / (pvpData.stats.wins + pvpData.stats.losses) * 100
-            else
-                pvpData.stats.winRate = 0
-            end
+            PvPDataLandDB[instanceID].players[name.."-"..(realm or GetRealmName())] = {
+                race = raceName,
+                class = className,
+                spec = specId,
+                talentTree = talentTree,
+                isEnemy = isEnemy,
+            }
         end
 
-        -- Add some debugging messages
-        print("PvPDataLand addon loaded.")
+        -- Store the start timestamp in the CombatLogData table
+        CombatLogData[startTimeStamp] = {
+            instanceID = instanceID,
+            events = {},
+        }
 
-        local function debugMessage(message)
-            if message then
-                DEFAULT_CHAT_FRAME:AddMessage("|cFF00FF00[PvPDataLand]|r " .. message)
-            end
-        end
+        -- Call GetPersonalRatedInfo to capture player's initial rating
+        GetPersonalRatedInfo()
+    elseif event == "COMBAT_LOG_EVENT_UNFILTERED" then
+        if currentInstanceID then
+            local timestamp, subevent, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, spellId, spellName, spellSchool, amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = CombatLogGetCurrentEventInfo()
 
-        local function addMatch(matchData)
-            -- Insert matchData into pvpData
-            debugMessage("Adding match: " .. tostring(matchData))
-
-            table.insert(pvpData.matches, matchData)
-
-            local mode = matchData.mode
-            table.insert(pvpData.modes[mode].matches, matchData)
-
-            updateStats()
-
-            -- Update the GUI with the new data for the selected mode
-            local modeDropdownValue = modeDropdown:GetValue()
-            local modeKey = modeDropdown:GetList()[modeDropdownValue]
-            updateGUI(pvpData.modes[modeKey])
-        end
-
-        -- Add some debugging messages
-        debugMessage("PvPDataLand addon loaded.")
-
-        SLASH_PVPDATA1 = "/pvpdata"
-        SlashCmdList["PVPDATA"] = function()
-            debugMessage("Slash command '/pvpdata' called.")
-            frame:Show()
-        end
-
-        -- Register combat log event
-        frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-        local combatData = {}
-        local function handleCombatLogEvent(timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, _, destGUID, destName, destFlags, _, spellId, spellName, _, amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand)
-            if subevent == "SPELL_DAMAGE" or subevent == "SPELL_PERIODIC_DAMAGE" or subevent == "SPELL_HEAL" or subevent == "SPELL_PERIODIC_HEAL" then
-                -- Store the data in the combatData table
-                combatData[#combatData + 1] = {
+            -- Store the combat log event in CombatLogData table
+            if CombatLogData[currentInstanceID] then
+                table.insert(CombatLogData[currentInstanceID].events, {
                     timestamp = timestamp,
                     subevent = subevent,
                     sourceGUID = sourceGUID,
                     sourceName = sourceName,
-                    destGUID = destGUID,
-                    destName = destName,
                     spellId = spellId,
-                    spellName = spellName,
                     amount = amount,
-                    overkill = overkill,
-                    school = school,
-                    resisted = resisted,
-                    blocked = blocked,
-                    absorbed = absorbed,
-                    critical = critical,
-                    glancing = glancing,
-                    crushing = crushing,
-                    isOffHand = isOffHand,
-                }
+                    spellName = spellName,
+                })
             end
         end
+    elseif event == "ARENA_MATCH_END" then
+        if currentInstanceID and PvPDataLandDB[currentInstanceID] then
+            local winningTeam, matchDuration, newRatingTeam1, newRatingTeam2 = ...
 
-        frame:SetScript("OnEvent", function(self, event, ...)
-            if event == "COMBAT_LOG_EVENT_UNFILTERED" then
-                handleCombatLogEvent(CombatLogGetCurrentEventInfo())
-            end
-            -- ... your existing event handling code
-        end)
+            -- Store the match end details
+            PvPDataLandDB[currentInstanceID].end = GetServerTime()
+            PvPDataLandDB[currentInstanceID].duration = matchDuration
+            PvPDataLandDB[currentInstanceID].winningTeam = winningTeam
+            PvPDataLandDB[currentInstanceID].newRatingTeam1 = newRatingTeam1
+            PvPDataLandDB[currentInstanceID].newRatingTeam2 = newRatingTeam2
+
+            -- Reset the current instance ID
+            currentInstanceID = nil
+        end
+    elseif event == "PERSONAL_RATED_INFO" then
+        if currentInstanceID and PvPDataLandDB[currentInstanceID] then
+            local rating = ...
+            PvPDataLandDB[currentInstanceID].startRating = rating
+        end
     end
 end)
+
+-- This function will handle errors (optional)
+frame:SetScript("OnError", function(self, error)
+    -- Handle the error, e.g., by logging it or displaying a message to the user
+    print(error)
+end)
+
+-- Function to retrieve combat log data for a specific instance ID
+local function GetCombatLogData(instanceID)
+    local combatLogs = {}
+    for timeStamp, data in pairs(CombatLogData) do
+        if data.instanceID == instanceID then
+            combatLogs[timeStamp] = data.events
+        end
+    end
+    return combatLogs
+end
